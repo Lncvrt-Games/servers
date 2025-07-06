338214854fdef37548ec5e48facdb68f0f0cb6cb
Lncvrt <lncvrtreal@gmail.com>
2025-07-06 16:07:18 -0700

    Fix beta version not being able to access servers

--- config/general.php
+++ config/general.php
@@ -1,5 +1,5 @@
 <?php
 $latestVersion = "1.5.2";
 $latestBetaVersion = "1.6-dev";
-$allowedVersions = [$latestVersion];
-$allowedDatabaseVersions = [$latestVersion];
+$allowedVersions = [$latestVersion, $allowedVersions];
+$allowedDatabaseVersions = [$latestVersion, $allowedVersions];
