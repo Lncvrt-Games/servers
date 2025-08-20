8b7f6b5b161c2a27f1b6b98262e3021610b882cd
Lncvrt <lncvrtreal@gmail.com>
2025-08-20 14:31:09 -0700

    Set latest version to 1.6

--- config/general.php
+++ config/general.php
@@ -1,6 +1,6 @@
 <?php
-$latestVersion = "1.5.2";
-$latestBetaVersion = "1.6-dev";
+$latestVersion = "1.6";
+$latestBetaVersion = $latestVersion;
 $latestLauncherVersion = "1.0.0";
 $allowedVersions = [$latestVersion, $latestBetaVersion];
 $allowedDatabaseVersions = [$latestVersion, $latestBetaVersion];
