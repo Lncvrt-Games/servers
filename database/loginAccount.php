fb0f3cc112b31b9830c45b0e5c5b24801e6e4a2f
Lncvrt <lncvrtreal@gmail.com>
2025-08-23 14:52:08 -0700

    Remove data for when you login

--- database/loginAccount.php
+++ database/loginAccount.php
@@ -8,7 +8,6 @@ $post = getPostData();
 $username = $post['username'];
 $password = $post['password'];
 $currentHighScore = $post['currentHighScore'] ?? 0;
-$loginType = $post['loginType'] ?? '0';
 
 $stmt = $conn->prepare("SELECT * FROM users WHERE username = ?");
 $stmt->bind_param("s", $username);
@@ -41,24 +40,6 @@ if ($currentHighScore > $user['highScore']) {
 }
 
 $data = ["session" => $token, "username" => $user['username'], "userid" => $id];
-
-if ($loginType === "0") {
-    $data += [
-        "highscore" => (string)$user['highScore'],
-        "icon" => (int)$user['icon'],
-        "overlay" => (int)$user['overlay'],
-        "totalNormalBerries" => (string)$user['totalNormalBerries'],
-        "totalPoisonBerries" => (string)$user['totalPoisonBerries'],
-        "totalSlowBerries" => (string)$user['totalSlowBerries'],
-        "totalUltraBerries" => (string)$user['totalUltraBerries'],
-        "totalSpeedyBerries" => (string)$user['totalSpeedyBerries'],
-        "totalCoinBerries" => (string)$user['totalCoinBerries'],
-        "totalAttempts" => (string)$user['totalAttempts'],
-        "birdColor" => json_decode((string)$user['birdColor']),
-        "overlayColor" => json_decode((string)$user['overlayColor']),
-        "marketplaceData" => json_decode((string)$user['marketplaceData'])
-    ];
-}
 
 echo encrypt(json_encode(["success" => true, "data" => $data]));
 
