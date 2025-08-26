8b36325b4c83181c48aec61f66f0fc37c17eb90e
Lncvrt <lncvrtreal@gmail.com>
2025-08-26 16:09:38 -0700

    Set latest version

--- config/general.php
+++ config/general.php
@@ -1,5 +1,5 @@
 <?php
-$latestVersion = "1.6";
+$latestVersion = "1.6.1";
 $latestBetaVersion = $latestVersion;
 $latestLauncherVersion = "1.0.0";
 $allowedVersions = [$latestVersion, $latestBetaVersion];
