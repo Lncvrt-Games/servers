96a1832e03b3761d3b945e8c4b9325d87cb02bd3
Lncvrt <lncvrtreal@gmail.com>
2025-08-26 19:14:04 -0700

    1.6.2

--- config/general.php
+++ config/general.php
@@ -1,5 +1,5 @@
 <?php
-$latestVersion = "1.6.1";
+$latestVersion = "1.6.2";
 $latestBetaVersion = $latestVersion;
 $latestLauncherVersion = "1.0.0";
 $allowedVersions = [$latestVersion, $latestBetaVersion];
