06286793665bc1c5d6534a55a4ebcc40208a735f
Lncvrt <lncvrtreal@gmail.com>
2025-08-29 10:24:45 -0700

    1.1.0 of the launcher

--- config/general.php
+++ config/general.php
@@ -1,6 +1,6 @@
 <?php
 $latestVersion = "1.6.3";
 $latestBetaVersion = $latestVersion;
-$latestLauncherVersion = "1.0.0";
+$latestLauncherVersion = "1.1.0";
 $allowedVersions = [$latestVersion, $latestBetaVersion, "1.6.2", "1.6.1", "1.6"];
 $allowedDatabaseVersions = [$latestVersion, $latestBetaVersion, "1.6.2", "1.6.1", "1.6", "1.2-beta2"];
