e15d5f997be370b02640395bf5bcffb05d25cbc8
Lncvrt <lncvrtreal@gmail.com>
2025-09-11 15:35:48 -0700

    1.8

--- config/general.php
+++ config/general.php
@@ -1,6 +1,6 @@
 <?php
-$latestVersion = "1.7.1";
-$latestBetaVersion = "1.8";
+$latestVersion = "1.8";
+$latestBetaVersion = $latestVersion;
 $latestLauncherVersion = "1.1.0";
-$allowedVersions = [$latestVersion, $latestBetaVersion, "1.7", "1.6.3", "1.6.2", "1.6.1", "1.6"];
-$allowedDatabaseVersions = [$latestVersion, $latestBetaVersion, "1.7", "1.6.3", "1.6.2", "1.6.1", "1.6", "1.3-beta2", "1.3-beta1", "1.21", "1.2", "1.2-beta2"];
+$allowedVersions = [$latestVersion, $latestBetaVersion, "1.7.1", "1.7", "1.6.3", "1.6.2", "1.6.1", "1.6"];
+$allowedDatabaseVersions = [$latestVersion, $latestBetaVersion, "1.7.1", "1.7", "1.6.3", "1.6.2", "1.6.1", "1.6", "1.3-beta2", "1.3-beta1", "1.21", "1.2", "1.2-beta2"];
