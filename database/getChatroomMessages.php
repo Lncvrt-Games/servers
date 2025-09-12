fe6fbf1c0795a567c147d500ab1a5b60f613c43d
Lncvrt <lncvrtreal@gmail.com>
2025-09-12 08:41:45 -0700

    Goddamnit

--- database/getChatroomMessages.php
+++ database/getChatroomMessages.php
@@ -1,12 +1,12 @@
 <?php
 require __DIR__ . '/../incl/util.php';
 setPlainHeader();
-// if (isAllowedDatabaseVersion(getClientVersion())) {
-    // if (getClientVersion() == "1.5.1" || getClientVersion() == "1.5.2") {
+if (isAllowedDatabaseVersion(getClientVersion())) {
+    if (getClientVersion() == "1.5.1" || getClientVersion() == "1.5.2") {
         require __DIR__ . '/backported/1.5.1/getChatroomMessages.php';
         exit;
-    // }
-// }
+    }
+}
 checkClientDatabaseVersion();
 $conn = newConnection();
 
