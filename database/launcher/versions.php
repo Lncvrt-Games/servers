894ca030adfd7cd6c61df067384195c7d5d43da0
Lncvrt <lncvrtreal@gmail.com>
2025-10-04 14:29:35 -0700

    Revert "Change the response"
    This reverts commit 10ae661d88320dbb23ca537433aa81cc94b6fd82.

--- database/launcher/versions.php
+++ database/launcher/versions.php
@@ -5,20 +5,18 @@ $conn = newConnection();
 
 $stmt = $conn->prepare("SELECT * FROM launcherversions WHERE hidden = 0 ORDER BY id DESC");
 $stmt->execute();
-$result = $stmt->get_result();
-$version_rows = $result->fetch_all(MYSQLI_ASSOC);
+
+$result_versions = $stmt->get_result();
+$versions = array_map(fn($row) => ['id' => $row['id'], 'version' => $row['version'], 'releaseDate' => $row['releaseDate'], 'displayName' => empty($row['displayName']) ? $row['version'] : $row['displayName'], 'platforms' => json_decode($row['platforms']), 'downloadUrls' => json_decode($row['downloadUrls']), 'executables' => json_decode($row['executables'])], $result_versions->fetch_all(MYSQLI_ASSOC));
+
 $stmt = $conn->prepare("SELECT * FROM launchercategories ORDER BY id DESC");
 $stmt->execute();
-$result = $stmt->get_result();
-$category_rows = $result->fetch_all(MYSQLI_ASSOC);
 
-$versions = [];
-foreach ($version_rows as $row) {
-    $versions[$row['id']] = ['version' => $row['version'], 'releaseDate' => $row['releaseDate'], 'displayName' => empty($row['displayName']) ? $row['version'] : $row['displayName'], 'platforms' => json_decode($row['platforms']), 'downloadUrls' => json_decode($row['downloadUrls']), 'executables' => json_decode($row['executables'])];
-}
+$result_categories = $stmt->get_result();
+$rows = $result_categories->fetch_all(MYSQLI_ASSOC);
 
 $categories = [];
-foreach ($category_rows as $row) {
+foreach ($rows as $row) {
     $categories[$row['id']] = $row['name'];
 }
 
