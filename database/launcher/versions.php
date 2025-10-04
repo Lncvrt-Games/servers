d72877d89638d786b21de2db8bd99e517b80e83c
Lncvrt <lncvrtreal@gmail.com>
2025-10-04 16:41:15 -0700

    I'm not steve, I'm retarded

--- database/launcher/versions.php
+++ database/launcher/versions.php
@@ -7,7 +7,7 @@ $stmt = $conn->prepare("SELECT * FROM launcherversions WHERE hidden = 0 ORDER BY
 $stmt->execute();
 
 $result_versions = $stmt->get_result();
-$versions = array_map(fn($row) => ['id' => $row['id'], 'version' => $row['version'], 'releaseDate' => $row['releaseDate'], 'displayName' => empty($row['displayName']) ? $row['version'] : $row['displayName'], 'platforms' => json_decode($row['platforms']), 'downloadUrls' => json_decode($row['downloadUrls']), 'executables' => json_decode($row['executables'])], $result_versions->fetch_all(MYSQLI_ASSOC));
+$versions = array_map(fn($row) => ['id' => $row['id'], 'version' => $row['version'], 'releaseDate' => $row['releaseDate'], 'displayName' => empty($row['displayName']) ? $row['version'] : $row['displayName'], 'platforms' => json_decode($row['platforms']), 'downloadUrls' => json_decode($row['downloadUrls']), 'executables' => json_decode($row['executables']), 'category' => $row['category']], $result_versions->fetch_all(MYSQLI_ASSOC));
 
 $stmt = $conn->prepare("SELECT * FROM launchercategories ORDER BY id DESC");
 $stmt->execute();
