f3815649341412ccd77b958508c4519aa8c58797
Lncvrt <lncvrtreal@gmail.com>
2025-10-13 11:18:39 -0700

    Fix launcher

--- launcher/latest.php
+++ launcher/latest.php
@@ -1,5 +1,5 @@
 <?php
-require __DIR__ . '/../../incl/util.php';
-require __DIR__ . '/../../config/general.php';
+require __DIR__ . '/../incl/util.php';
+require __DIR__ . '/../config/general.php';
 setPlainHeader();
 echo $latestLauncherVersion;
--- launcher/versions.php
+++ launcher/versions.php
@@ -1,5 +1,5 @@
 <?php
-require __DIR__ . '/../../incl/util.php';
+require __DIR__ . '/../incl/util.php';
 setJsonHeader();
 $conn = newConnection();
 
